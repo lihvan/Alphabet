c2fb2ed721eec167cc907f811d9c177db9cfaba4
LimorenkoNikita64 <lim.nikita.nik@gmail.com>
2020-11-20 13:40:58 +0700

    The text from the file is added to the container (vector).

--- src/alphabet.cpp
+++ src/alphabet.cpp
@@ -11,7 +11,16 @@ int alphabet::ReadFromFile(std::string path)
     std::ifstream file;
     file.open(path, std::ios::in);
     if (file.is_open()) {
-        return 1;
+        if (file.good()) {
+            std::string conteiner, word;
+            while (getline(file, conteiner)) {
+                std::istringstream ss(conteiner);
+                while (ss >> word) {
+                    data.push_back(word);
+                }
+            }
+            return 1;
+        }
     }
     return 0;
 }
