4826fa956e935f7c365f148a8ef36bcc0e83c647
LimorenkoNikita64 <lim.nikita.nik@gmail.com>
2020-11-20 12:34:48 +0700

    Test (open file) been added.

---
+++ test/alphabet_test.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN
+#include "alphabet.h"
+#include "catch.hpp"
+
+TEST_CASE("File_Is_Open", "[ReadFile]")
+{
+    alphabet testingClass;
+    std::string path = "../test/any_text.txt";
+    int expected = 1;
+    int result = testingClass.ReadFromFile(path);
+    REQUIRE(expected == result);
+}
+
+TEST_CASE("File_Not_Open", "[ReadFile]")
+{
+    alphabet testingClass;
+    std::string path = "../test/non-existent.txt";
+    int expected = 0;
+    int result = testingClass.ReadFromFile(path);
+    REQUIRE(expected == result);
+}
