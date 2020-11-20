55d479f55afded55325449ee4eb1b438587e9dc3
LimorenkoNikita64 <lim.nikita.nik@gmail.com>
2020-11-20 18:08:31 +0700

    In column output function has been added

--- src/show.cpp
+++ src/show.cpp
@@ -7,3 +7,11 @@ void alphabet::ShowInLine()
     }
     std::cout << std::endl;
 }
+
+void alphabet::ShowInColumn()
+{
+    for (size_t i = 0; i < data.size(); i++) {
+        std::cout << data[i] << "\n";
+    }
+    std::cout << std::endl;
+}
